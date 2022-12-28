fb7b3297dcc543e24c1893519d7db8a39bcab80f
Bryant Biggs <bryantbiggs@gmail.com>
2022-12-28 14:27:11 -0500

    chore: Init to winit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use anyhow::*;
+
+use kube::{
+    api::{Api, DynamicObject, ResourceExt},
+    discovery::{verbs, Discovery, Scope},
+    Client,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let client = Client::try_default().await?;
+
+    let discovery = Discovery::new(client.clone()).run().await?;
+    for group in discovery.groups() {
+        for (ar, caps) in group.recommended_resources() {
+            if !caps.supports_operation(verbs::LIST) {
+                continue;
+            }
+            let api: Api<DynamicObject> = if caps.scope == Scope::Cluster {
+                Api::all_with(client.clone(), &ar)
+            } else {
+                Api::default_namespaced_with(client.clone(), &ar)
+            };
+
+            println!("{}/{} : {}", group.name(), ar.version, ar.kind);
+
+            let list = api.list(&Default::default()).await?;
+            for item in list.items {
+                let name = item.name_any();
+                let ns = item.metadata.namespace.map(|s| s + "/").unwrap_or_default();
+                println!("\t\t{}{}", ns, name);
+            }
+        }
+    }
+
+    Ok(())
+}
