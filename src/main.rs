64104c5e5221a54b59bcab7883b9f5e53c5ac0ad
Bryant Biggs <bryantbiggs@gmail.com>
2022-12-28 19:34:33 -0500

    feat: Add current version collection from discovery API

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,15 @@
-mod deprecation;
+mod versions;
 
-use anyhow::*;
-
-// use kube::{
-//     api::{Api, DynamicObject, ResourceExt},
-//     discovery::{verbs, Discovery, Scope},
-//     Client,
-// };
+use kube::Client;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let deprecations = deprecation::Deprecations::new()?;
-    println!("{deprecations:#?}");
+    let deprecated_versions = versions::Deprecated::get()?;
+    println!("{deprecated_versions:#?}");
 
-    // let client = Client::try_default().await?;
-
-    // let discovery = Discovery::new(client.clone()).run().await?;
-    // for group in discovery.groups() {
-    //     for (ar, caps) in group.recommended_resources() {
-    //         if !caps.supports_operation(verbs::LIST) {
-    //             continue;
-    //         }
-    //         let api: Api<DynamicObject> = if caps.scope == Scope::Cluster {
-    //             Api::all_with(client.clone(), &ar)
-    //         } else {
-    //             Api::default_namespaced_with(client.clone(), &ar)
-    //         };
-
-    //         println!("{}/{} : {}", group.name(), ar.version, ar.kind);
-
-    //         let list = api.list(&Default::default()).await?;
-    //         for item in list.items {
-    //             let name = item.name_any();
-    //             let ns = item.metadata.namespace.map(|s| s + "/").unwrap_or_default();
-    //             println!("\t\t{}{}", ns, name);
-    //         }
-    //     }
-    // }
+    let client = Client::try_default().await?;
+    let current_versions = versions::Current::get(&client).await?;
+    println!("{current_versions:#?}");
 
     Ok(())
 }
---
+++ src/versions.rs
@@ -0,0 +1,99 @@
+use kube::{discovery::Discovery, Client};
+
+use rust_embed::RustEmbed;
+use serde::Deserialize;
+
+// A version holds the information about a current API version
+#[derive(Deserialize, Debug)]
+pub struct Version {
+    // Name of the API version
+    pub api_version: String,
+    // Kind of the object associated with this version
+    pub kind: String,
+    // DeprecatedIn indicates what version the API is deprecated in
+    // an empty string indicates that the version is not deprecated
+}
+
+// A version holds the information about a deprecated API version and potential replacement API
+#[derive(Deserialize, Debug)]
+pub struct DeprecatedVersion {
+    // Name of the API version
+    pub api_version: String,
+    // Kind of the object associated with this version
+    pub kind: String,
+    // DeprecatedIn indicates what version the API is deprecated in
+    // an empty string indicates that the version is not deprecated
+    pub deprecated_in: String,
+    // RemovedIn denotes the version that the api was actually removed in
+    // `None` indicates that the version has not been removed yet
+    pub removed_in: String,
+    // ReplacementAPI is the apiVersion that replaces the deprecated one
+    pub replacement_api_version: Option<String>,
+}
+
+// Holds the deprecated API versions
+#[derive(Deserialize, Debug)]
+pub struct Deprecated {
+    pub versions: Vec<DeprecatedVersion>,
+}
+
+#[derive(RustEmbed)]
+#[folder = "assets/"]
+struct Assets;
+
+impl Deprecated {
+    pub fn get() -> Result<Self, anyhow::Error> {
+        let deprecation_file = Assets::get("deprecations.yaml").unwrap();
+        let contents = std::str::from_utf8(deprecation_file.data.as_ref()).unwrap();
+        let deprecations: Deprecated = serde_yaml::from_str(contents)?;
+
+        Ok(deprecations)
+    }
+}
+
+// Holds the deprecated API versions
+#[derive(Deserialize, Debug)]
+pub struct Current {
+    pub versions: Vec<Version>,
+}
+
+impl Current {
+    pub async fn get(client: &Client) -> Result<Self, anyhow::Error> {
+        let mut versions = Vec::new();
+
+        let discovery = Discovery::new(client.clone()).run().await?;
+        for group in discovery.groups() {
+            for (res, _caps) in group.recommended_resources() {
+                // if !caps.supports_operation(verbs::LIST) {
+                //     continue;
+                // }
+                // let api: Api<DynamicObject> = if caps.scope == Scope::Cluster {
+                //     Api::all_with(client.clone(), &ar)
+                // } else {
+                //     Api::default_namespaced_with(client.clone(), &ar)
+                // };
+
+                let api_version = if group.name() == "" {
+                    // "core" group does not have a group name, its left blank and only the version is used
+                    res.version
+                } else {
+                    format!("{}/{}", group.name(), res.version)
+                };
+
+                versions.push(Version {
+                    api_version,
+                    kind: res.kind,
+                })
+
+                // let list = api.list(&Default::default()).await?;
+                // for item in list.items {
+                //     let name = item.name_any();
+                //     let ns = item.metadata.namespace.map(|s| s + "/").unwrap_or_default();
+                //     println!("\t\t{}{}", ns, name);
+                // }
+            }
+        }
+
+        Ok(Current { versions })
+    }
+}
